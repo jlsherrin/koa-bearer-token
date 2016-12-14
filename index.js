4d72237afc5ad2b8f507eaf99a5b363127da477c
Walberto Ibarra <waldo.ibarra92@gmail.com>
2016-12-14 16:55:44 -0600

    Wrap it all in a promise

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-'use strict'
+'use strict';
 
-module.exports = function(opts) {
-  opts = opts || {}
+module.exports = function (opts) {
+	opts = opts || {};
 
-  var queryKey = opts.queryKey || 'access_token'
-  var bodyKey = opts.bodyKey || 'access_token'
-  var headerKey = opts.headerKey || 'Bearer'
-  var reqKey = opts.reqKey || 'token'
+	var queryKey = opts.queryKey || 'access_token';
+	var bodyKey = opts.bodyKey || 'access_token';
+	var headerKey = opts.headerKey || 'Bearer';
+	var reqKey = opts.reqKey || 'token';
 
-  return function (ctx, next) {
-    var token, err
+	return function (ctx, next) {
+		return new Promise(resolve => {
+			var token, err;
 
-    var r = ctx.request
+			var r = ctx.request;
 
-    var query = r.query
-    var body = r.body
-    var header = r.header
+			var query = r.query;
+			var body = r.body;
+			var header = r.header;
 
-    if (query && query[queryKey]) {
-      token = query[queryKey]
-    }
+			if (query && query[queryKey])
+				token = query[queryKey];
 
-    if (body && body[bodyKey]) {
-      if (token) {
-        err = true
-      }
-      token = body[bodyKey]
-    }
+			if (body && body[bodyKey]) {
+				if (token)
+					err = true;
 
-    if (header && header.authorization) {
-      var parts = header.authorization.split(' ')
-      if (parts.length === 2 && parts[0] === headerKey) {
-        if (token) {
-          err = true
-        }
-        token = parts[1]
-      }
-    }
+				token = body[bodyKey];
+			}
 
-    // RFC6750 states the access_token MUST NOT be provided
-    // in more than one place in a single request.
-    if (err) {
-      ctx.throw(400, 'token_invalid', {
-        message: 'access_token MUST NOT be provided in more than one place'
-      })
-    } else {
-      r[reqKey] = token
-      next();
-    }
+			if (header && header.authorization) {
+				var parts = header.authorization.split(' ');
+				if (parts.length === 2 && parts[0] === headerKey) {
+					if (token)
+						err = true;
 
-  }
+					token = parts[1];
+				}
+			}
+
+			// RFC6750 states the access_token MUST NOT be provided
+			// in more than one place in a single request.
+			if (err) {
+				ctx.throw(400, 'token_invalid', {
+					message: 'access_token MUST NOT be provided in more than one place'
+				})
+			} else {
+				r[reqKey] = token;
+				resolve(next());
+			}
+		});
+	}
 }
